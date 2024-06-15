f8fa30eb265eee1f0ba1cd8d46bf2f6e4523c17c
raghuprasadks <prasadraghuks@gmail.com>
2024-06-15 11:50:17 +0530

    updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'welcome to flask with cicd'
+    return 'welcome to flask with cicd/cd pipeline!'
 
 if __name__ == '__main__':
     app.run()
